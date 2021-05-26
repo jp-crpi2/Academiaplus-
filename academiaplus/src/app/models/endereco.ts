c82c6d109616b8d78f318a64d3c8696c927d39de
Jotape <jpcrpisenac@gmail.com>
2021-05-26 20:20:29 -0300

    aula26/05/2021

---
+++ academiaplus/src/app/models/endereco.ts
@@ -0,0 +1,12 @@
+export class User {
+numero: string;
+    complemento: string;
+    
+    cep: string;
+    logradouro: string;
+    bairro: string;
+    localidade: string;
+    uf: string;
+
+    erro:boolean = false;
+}
